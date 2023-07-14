4ba8104a89a5406c77f289dfed651f4fb5cbcb1d
AI-World-CEO <128266278+AI-World-CEO@users.noreply.github.com>
2023-07-14 11:09:22 -0700

    Commit message for your changes

--- src/main.py
+++ src/main.py
@@ -1,19 +1,10 @@
 # main.py
 import os
 from src.pipeline import Pipeline
-from utilities import create_directories
 
 
 def main(pdf_path: str, output_path: str):
     print("Initializing pipeline...")
-
-    # Create the necessary directories before running the pipeline
-    directories = ['data/compressed', 'data/decompressed', 'data/raw',
-                   'src/compression', 'src/preprocessing',
-                   'src/evaluation', 'models', 'results',
-                   'src/model', 'src/training']
-    create_directories(directories)
-
     pipeline = Pipeline()
     print("Running pipeline...")
     result = pipeline.run_pipeline(pdf_path)
@@ -24,22 +15,13 @@ def main(pdf_path: str, output_path: str):
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
     # Save the compressed dataset into a file
-    with open(output_path, "w") as file:
-        file.write(result)
+    if result is not None:  # Add a check to ensure result is not None before writing to a file
+        with open(output_path, "w") as file:
+            file.write(result)
+    else:
+        print("No results to write to file.")
 
 
 if __name__ == "__main__":
-    if 'COLAB_GPU' in os.environ:
-        # Replace this with the path to your pdf file in Google Drive
-        path_to_pdf = '/content/drive/My Drive/<Your Directory>/<Your PDF File>.pdf'
-
-        # Replace this with the path to your desired output directory in Google Drive
-        path_to_output = '/content/drive/My Drive/<Your Directory>/compressed_dataset.txt'
-    else:
-        # Replace this with the path to your pdf file on your local machine
-        path_to_pdf = "<Your Directory>/<Your PDF File>.pdf"
-
-        # Replace this with the path to your desired output directory on your local machine
-        path_to_output = "<Your Directory>/compressed_dataset.txt"
-
-    main(path_to_pdf, path_to_output)
+    main("C:\\Users\\Garry Anderson\\Music\\sim2940_technical.pdf",
+         "C:\\output\\compressed_dataset.txt")
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,15 +1,16 @@
-from typing import Optional
+from textbook_scanner import TextbookScanner
+from text_processor import TextProcessor
 from dataset_creator import DatasetCreator
-from src.logger import my_logger as logger
-from src.compression_utility import CompressionUtility
-from src.text_processor import TextProcessor
-from src.textbook_scanner import TextbookScanner
+from compression_utility import CompressionUtility
+from src.logger import setup_logger  # Assuming the logger setup function is available in the src.logger module
 
 
 class Pipeline:
 
-    @staticmethod
-    def run_pipeline(pdf_path: str) -> Optional[str]:
+    def __init__(self):
+        self.logger = setup_logger('pipeline_logger', 'pipeline.log')  # Set up a logger for the pipeline class
+
+    def run_pipeline(self, pdf_path: str) -> str:
         try:
             scanner = TextbookScanner()
             processor = TextProcessor()
@@ -17,18 +18,15 @@ class Pipeline:
             utility = CompressionUtility()
 
             text = scanner.scan_textbook(pdf_path)
-            processed_text = processor.process_text_advanced(text)
+            processed_text = processor.process_text_advanced(text)  # Using the correct method from TextProcessor
             dataset = creator.create_dataset(processed_text)
             compressed_dataset = utility.compress_dataset(dataset)
 
             return compressed_dataset
 
         except FileNotFoundError:
-            logger.error(f"File not found: {pdf_path}")
-            return None
-        except PermissionError:
-            logger.error(f"No permission to read file: {pdf_path}")
+            self.logger.error(f"File not found: {pdf_path}")  # Using the logger to log errors
             return None
         except Exception as e:
-            logger.error(f"An error occurred: {str(e)}")
+            self.logger.error(f"An error occurred: {str(e)}")  # Using the logger to log exceptions
             return None
